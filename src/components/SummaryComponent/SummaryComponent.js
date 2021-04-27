91bb4bac260bfb88c5d98c4ea478ec01a02d54f0
Enqira <moenkira@yahoo.com>
2021-04-28 01:40:06 +0200

    finished summary component

---
+++ src/components/SummaryComponent/SummaryComponent.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Summary.styles.css"
+
+export default function SummaryComponent() {
+  return (
+    <div className="summary-container">
+      <div className="summary-card card">
+        <h2>Summary</h2>
+        <h3>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make{" "}
+        </h3>
+      </div>
+    </div>
+  )
+}
