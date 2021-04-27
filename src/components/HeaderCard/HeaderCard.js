a60123dcf3bdc6fa5c07518ee43820486561d56c
Enqira <moenkira@yahoo.com>
2021-04-28 00:41:55 +0200

    finished header component and added logos

--- src/components/HeaderCard/HeaderCard.js
+++ src/components/HeaderCard/HeaderCard.js
@@ -1,6 +1,9 @@
 import React, { useRef, useState, useEffect } from "react"
 import "./HeaderCard.css"
 import profileImg from "../../images/profile-img.png"
+import githubLogo from "../../images/git-logo.png"
+import linkedinLogo from "../../images/linkedin-logo.png"
+import emailLogo from "../../images/email-logo.png"
 
 export default function HeaderCard() {
   //   const [xAxes, setXAxes] = useState(null)
@@ -16,8 +19,8 @@ export default function HeaderCard() {
   useEffect(() => {
     //   add event listener of mouse movement inside container
     containerRef.current.addEventListener("mousemove", e => {
-      const xAxes = -((window.innerWidth / 2 - e.pageX) / 50)
-      const yAxes = (window.innerHeight / 2 - e.pageY) / 50
+      const xAxes = -((window.innerWidth / 2 - e.pageX) / 80)
+      const yAxes = (window.innerHeight / 2 - e.pageY) / 80
       setCardStyle({
         transform: `rotateX(${yAxes}deg) rotateY(${xAxes}deg)`
       })
@@ -27,12 +30,14 @@ export default function HeaderCard() {
     containerRef.current.addEventListener("mouseenter", e => {
       picRef.current.style.transform =
         "scaleX(0.9) scaleY(0.9) translateZ(50px)"
-      titleRef.current.style.transform = "translateZ(150px)"
+      //   titleRef.current.style.boxShadow =
+      //     "0 20px 20px rgba(0, 0, 0, 0.2), 0 0 50px rgba(0, 0, 0, 0.2)"
+      titleRef.current.style.transform = "translateZ(200px)"
       descriptionRef.current.style.transform = "translateZ(80px)"
       btnRef.current.style.transform = "translateZ(100px)"
 
-      picBackRef.current.style.width = "0"
-      picBackRef.current.style.height = "0"
+      picBackRef.current.style.width = "155px"
+      picBackRef.current.style.height = "155px"
     })
 
     // add event listener to make card turn to initial rotation when mouse leavs container
@@ -43,6 +48,7 @@ export default function HeaderCard() {
       })
       //   picRef.current.style.transform = "translateZ(1px)"
       picRef.current.style.transform = "scaleX(0.8) scaleY(0.8) translateZ(0px)"
+      //   titleRef.current.style.boxShadow = "none"
 
       titleRef.current.style.transform = "translateZ(0px)"
       descriptionRef.current.style.transform = "translateZ(0px)"
@@ -71,16 +77,16 @@ export default function HeaderCard() {
         </div>
         <div className="header-info">
           <h1 className="header-title" ref={titleRef}>
-            Mohammed Enqira
+            MOHAMMED ENQIRA
           </h1>
           <h3 ref={descriptionRef}>
             Very motivated, hard worker and fast learner Full stack developer
             with a strong will to solve real world problems and build projects.{" "}
           </h3>
           <div className="header-links" ref={btnRef}>
-            <button>github</button>
-            <button>linkedin</button>
-            <button>email</button>
+            <img src={githubLogo} alt="github" />
+            <img src={linkedinLogo} alt="linkedin" />
+            <img src={emailLogo} alt="email" />
           </div>
         </div>
       </div>
