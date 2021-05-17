ae1d2c606d12dd256510f05bacd79cda7d2d4984
Enqira <53911763+Enqira@users.noreply.github.com>
2021-05-17 15:01:04 +0200

    implemented TwoLines component in all cards

--- src/components/JobExperience/JobExperienceComponent.js
+++ src/components/JobExperience/JobExperienceComponent.js
@@ -1,25 +1,26 @@
 import React from "react"
 import "./JobExperienceComponent.css"
+import TwoLines from "../TwoLines/TwoLinesComponent"
 
 export default function JobExperienceComponent() {
   return (
     <dir className="job-experience-container">
       <div className="card">
         <h2>Job Experience</h2>
-        <h3>Fixpress</h3>
+        <TwoLines title={"Fixpress"} />
         <div>
           <p>
             {`Fixpress was a small computer & mobile repair shop. My role was to repair electronics and attend clients.`}
           </p>
         </div>
-        <h3>PcBox/Let's Play</h3>
+        <TwoLines title={"PcBox/Let's Play"} />
         <div>
           <p>
             {`
 PcBox/Let's Play is an Esport center with 110 robust gaming computers and 8 PlayStations. My role consisted of help build and maintain the center computers, install and patch games, and fix issues that could occur in the network.`}
           </p>
         </div>
-        <h3>Brink Transport</h3>
+        <TwoLines title={"Brink Transport"} />
         <div>
           <p>
             Brink transport is a danish transport company. My daily activities
